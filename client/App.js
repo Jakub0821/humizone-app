d6373568ce67ba8e56ebb41dc21bc04e06850823
Jakub0821 <jakubmigda120@gmail.com>
2025-08-30 18:54:24 +0200

    change

---
+++ client/App.js
@@ -0,0 +1,13 @@
+import './App.css'; // bez "src/"
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>aplikacja humizone</h1>
+      <p>Witamy na naszej stronie!</p>
+    </div>
+  );
+}
+
+export default App;
