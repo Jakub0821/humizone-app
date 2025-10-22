2769b6a3d1e753231e9db827e67c367789796708
Jakub0821 <jakubmigda120@gmail.com>
2025-10-22 14:23:16 +0200

    change

---
+++ platform/client/src/views/Groups.js
@@ -0,0 +1,11 @@
+
+function Groups() {
+  return (
+    <div>
+      <h1>Grupy</h1>
+      <p>Tu możesz dołączyć do grup tematycznych lub utworzyć własną.</p>
+    </div>
+  );
+}
+
+export default Groups;
--- platform/client/src/views/Home.js
+++ platform/client/src/views/Home.js
@@ -1,6 +1,8 @@
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      {/* zawartość strony domowej */}
+    </div>
   );
 }
 
