75cf1e9d0bbdb234f07498a78d44de3c921a4b6e
Jakub0821 <jakubmigda120@gmail.com>
2025-11-07 22:01:42 +0100

    change

--- platform/client/src/views/Login.js
+++ platform/client/src/views/Login.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import '../css/Login.css'; // Styl oddzielny
+
 function Login() {
   return (
-    <div>
-      {/* zawartość strony domowej */}
+    <div className="login-container">
+      <div className="login-left">
+        <h1>Humizone</h1>
+        <p>Zaloguj się, aby kontynuować</p>
+      </div>
+      <div className="login-right">
+        <h2>Logowanie</h2>
+        <form>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Hasło" />
+          <button type="submit">Zaloguj się</button>
+        </form>
+      </div>
     </div>
   );
 }
