64c719e92250232109207c27ed8a8b7783c5c753
Jakub0821 <jakubmigda120@gmail.com>
2025-11-30 20:42:43 +0100

    change

--- platform/client/src/views/Home.js
+++ platform/client/src/views/Home.js
@@ -1,9 +1,25 @@
-function Home() {
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
+
+const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      {/* zawartość strony domowej */}
+    <div className="home-container">
+      <div className="home-content">
+        <h1>Witaj w <span className="brand">Humizone</span></h1>
+        <p>Twoja strefa zadań, czatu i grup</p>
+        <div className="home-buttons">
+          <button onClick={() => navigate("/logowanie")}>Zaloguj się</button>
+          <button onClick={() => navigate("/rejestracja")}>Zarejestruj się</button>
+        </div>
+      </div>
+      <div className="home-image">
+        <img src="https://undraw.co/api/illustrations/choose-your-path.svg" alt="Ilustracja" />
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
--- platform/client/src/views/NavBar.js
+++ platform/client/src/views/NavBar.js
@@ -1,16 +1,20 @@
-import '../css/Navbar.css';
-import { Link } from 'react-router-dom';
+import "../css/Navbar.css";
+import { Link } from "react-router-dom";
+import logo from "../assets/HumiZone.png";
 
 function Navbar() {
   return (
-    <nav>
-      <h2>Humizone</h2>
-      <ul>
-        <li><Link to="/Home">Home</Link></li>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo Humizone" />
+        <h2>Humizone</h2>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/home">Home</Link></li>
         <li><Link to="/czat">Czat</Link></li>
         <li><Link to="/zadania">Zadania</Link></li>
         <li><Link to="/logowanie">Logowanie</Link></li>
-        <li><Link to="/grupy">Grupy</Link></li> 
+        <li><Link to="/grupy">Grupy</Link></li>
       </ul>
     </nav>
   );
